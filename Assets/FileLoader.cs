47ea83c2b362ee8b48d43ed2b997ba3b5cd99ac0
ejohnsonflex <60490583+ejohnsonflex@users.noreply.github.com>
2020-09-19 15:09:43 +0100

    Read CLS data from file
    File.ReadAllLines

---
+++ Assets/FileLoader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileLoader : MonoBehaviour
+{
+    public class CLSData
+    {
+        private const string path = @"Assets/config.dat";
+  
+        private static string CLSID;
+        private static string feature;
+        private static string version;
+        private static string hostID;
+        private static string vendorDictionary;
+
+        public static void Parse()
+        {
+            var cLSData = new CLSData();
+           
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+                foreach (var line in lines)
+                {
+                    Debug.Log(line.ToString());
+                }
+            }
+
+            catch(Exception e)
+            {
+                Debug.Log(e.Message.ToString());
+            }
+
+            
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        CLSData.Parse();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
